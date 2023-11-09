7de982d6df6d9dfa5f336393a94b95fda901ff81
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-11-09 10:34:00 +0700

    Refactor check-canary.js to improve stability and performance

--- scripts/check-canary.js
+++ scripts/check-canary.js
@@ -5,16 +5,13 @@ const packageName = 'i18n-num-in-words';
 const getLastStableVersionDate = () => {
   // Get all versions including time to find the latest stable release date
   const output = execSync(`npm view ${packageName} time --json`);
-  console.debug(`output: ${output}`);
   const times = JSON.parse(output);
 
   // Ignore pre-releases and sort by semver to get the latest version
   const stableVersions = Object.keys(times).filter(
     (version) => semver.valid(version) && !semver.prerelease(version)
   );
-  console.debug(`stableVersions: ${stableVersions}`);
   const latestStableVersion = stableVersions.sort(semver.rcompare)[0];
-  console.debug(`latestStableVersion: ${latestStableVersion}`);
 
   return new Date(times[latestStableVersion]);
 };
@@ -24,20 +21,16 @@ const hasCanaryReleaseSinceLastStable = () => {
 
   // Get all versions
   const allVersions = execSync(`npm view ${packageName} versions --json`);
-  console.debug(`allVersions: ${allVersions}`);
   const versions = JSON.parse(allVersions);
-  console.debug(`versions: ${versions}`);
 
   // Check if there is a canary version newer than the last stable version
   return versions.some((version) => {
     const isCanary = version.includes('canary');
-    console.debug(`isCanary: ${isCanary}`);
     if (!isCanary) return false;
 
     const versionDate = new Date(
       JSON.parse(execSync(`npm view ${packageName} time --json`))[`${version}`]
     );
-    console.debug(`versionDate: ${versionDate}`);
     return versionDate > lastStableVersionDate;
   });
 };
