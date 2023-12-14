c7c88f849fb6833f4a6b8c2c2df03e940fdbf43c
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-12-14 19:08:39 +0700

    Refactor build and publish process

---
+++ scripts/build-and-publish.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+VERSION=$(node scripts/determine-next-version.js)
+echo "Building and publishing version $VERSION"
+DEFAULT_LAST_TAG=$(node scripts/get-default-last-tag.js)
+echo "Default last tag is $DEFAULT_LAST_TAG"
+
+# Set the version and default last tag as environment variables
+echo "VERSION=$VERSION" >>$GITHUB_ENV
+echo "DEFAULT_LAST_TAG=$DEFAULT_LAST_TAG" >>$GITHUB_ENV
+
+# Build the library
+cd library
+bun run build
+
+# Set the Git user
+echo "Setting Git user"
+git config user.name github-actions
+git config user.email github-actions@github.com
+
+# Get last tag name to compare the history
+echo "Getting last tag name"
+LAST_TAG=$(git tag | tail -1)
+if [ -z "$LAST_TAG" ]; then
+  LAST_TAG="$DEFAULT_LAST_TAG"
+fi
+echo "Last tag is $LAST_TAG"
+
+# Generate release notes from commits
+echo "Generating release notes"
+echo "RELEASE_NOTES=$(git log ${LAST_TAG}..HEAD --pretty=format:'* %s (%h)' --reverse)" >>$GITHUB_ENV
+echo "RELEASE_NOTES=$(echo $RELEASE_NOTES | sed 's/"/\\"/g')"
+
+# Push the tag to the repository
+git push origin $VERSION
+
+yarn version --new-version $VERSION
+yarn publish --tag latest --provenance
