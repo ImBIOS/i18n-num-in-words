94a16440cb04fbc023428c8ba0a140968dc14a6d
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-12-14 19:50:28 +0700

    Update npm publish command
    * Update npm publish command

--- scripts/build-and-publish.sh
+++ scripts/build-and-publish.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e # Exit immediately if a command exits with a non-zero status.
 
 VERSION=$(node scripts/determine-next-version.js)
 echo "Building and publishing version $VERSION"
@@ -15,12 +16,12 @@ bun run build
 
 # Set the Git user
 echo "Setting Git user"
-git config user.name github-actions
-git config user.email github-actions@github.com
+git config user.name "github-actions"
+git config user.email "github-actions@github.com"
 
 # Get last tag name to compare the history
 echo "Getting last tag name"
-LAST_TAG=$(git tag | tail -1)
+LAST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1)) # Get the latest tag based on commit date
 if [ -z "$LAST_TAG" ]; then
   LAST_TAG="$DEFAULT_LAST_TAG"
 fi
@@ -28,11 +29,16 @@ echo "Last tag is $LAST_TAG"
 
 # Generate release notes from commits
 echo "Generating release notes"
-echo "RELEASE_NOTES=$(git log --pretty=format:"%s" $LAST_TAG..HEAD)" >>$GITHUB_ENV
-echo "RELEASE_NOTES=$(echo $RELEASE_NOTES | sed 's/"/\\"/g')"
+RELEASE_NOTES=$(git log --pretty=format:"%s" $LAST_TAG..HEAD | sed 's/"/\\"/g')
+echo "RELEASE_NOTES=$RELEASE_NOTES" >>$GITHUB_ENV
+
+# Update the package.json version
+yarn version --new-version $VERSION
+
+# Commit and push changes if needed
+git commit -am "Release $VERSION"
+git push origin main
 
 # Push the tag to the repository
+git tag $VERSION
 git push origin $VERSION
-
-yarn version --new-version $VERSION
-npm publish --provenance --access public --tag latest
